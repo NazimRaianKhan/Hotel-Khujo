dd993b4ac889e9b8c9a802037d1a9fe7c408e456
Yahya <takeyahya6@gmail.com>
2024-05-22 22:08:37 +0600

    reg page added

---
+++ lib/reg.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          elevation: 0,
+        ),
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 35, top: 70),
+              child: Text('Create\nAccount', style: TextStyle(
+                color: Colors.deepOrange,
+                fontSize: 33,
+              ),),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.28,
+                    right: 35,
+                    left: 35),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: 'Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              color: Colors.orange,
+                              fontSize: 27,
+                              fontWeight: FontWeight.w700),
+                        ),
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Colors.orange[200],
+                          child: IconButton(
+                            onPressed: (){},
+                            icon: Icon(Icons.arrow_forward),
+                          ),
+                        )
+                      ],
+                    ),
+
+
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
