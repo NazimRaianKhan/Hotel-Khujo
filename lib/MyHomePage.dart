2be434557b863aaa33208f2365edc4af629b017f
Dhruvo <aribur.cse.20220204003@aust.edu>
2024-06-01 22:24:07 +0600

    Update MyHomePage.dart

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,9 +1,26 @@
 /*
  * @author: ARDhruvo
-*/
+ * Update 1: Search Bar
+ */
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
 class MyHomePage extends StatefulWidget {
   final String title;
@@ -30,12 +47,20 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(239,108,0, 1),
-      ),
+      appBar: AppBar(backgroundColor: Color.fromRGBO(239, 108, 0, 1), actions: [
+        IconButton(
+          onPressed: () {
+            showSearch(
+              context: context,
+              delegate: CustomSearchDelegate(),
+            );
+          },
+          icon: const Icon(Icons.search),
+        ),
+      ]),
       drawer: Drawer(
         child: Container(
-            color: const Color.fromRGBO(255,183,77, 1),
+            color: const Color.fromRGBO(255, 183, 77, 1),
             child: ListView(
               children: [
                 DrawerHeader(child: Center(child: Text("Insert Icon here"))),
@@ -84,6 +109,75 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+  List<String> hotels = [
+    'Pan Pacific',
+    'Intercontinental',
+    'Westin',
+    'Radison',
+    'Sea Pearl'
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+          }),
+    ];
+  }
+
+  @override
+  List<Widget> buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  List<Widget> buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var hot in hotels) {
+      if (hot.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(hot);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+  @override
+  List<Widget> buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var hot in hotels) {
+      if (hot.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(hot);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
     );
   }
 }
