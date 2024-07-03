35307041cbe82a45e538552bd783548eda43b5d7
Yahya <takeyahya6@gmail.com>
2024-07-03 23:57:57 +0600

    details modified

--- lib/Hotel/HotelDetailPage.dart
+++ lib/Hotel/HotelDetailPage.dart
@@ -9,16 +9,42 @@ class HotelDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(hotel.name),
-      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.network(hotel.imageUrl),
           SizedBox(height: 20),
-          Text('Details about ${hotel.name} can be shown here.'),
+          Text(' ${hotel.name} '
+              '\n107 Kazi Nazrul Islam Avenue, Ramna, Dhaka, Bangladesh, 1215 - See map'
+              '\nThe car parking and the Wi-Fi are always free, so you can stay in touch and come and go as you please. Conveniently situated in the Ramna part of Dhaka, this property puts you close to attractions and interesting dining options. Dont leave before paying a visit to the famous Shahjalal International Airport. Rated with 5 stars, this high-quality property provides guests with access to massage, restaurant and hot tub on-site.'),
           // Add more details or actions as needed
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: ElevatedButton(
+              onPressed: () async{
+              },
+              style: ElevatedButton.styleFrom(
+                elevation: 10,
+                backgroundColor: Colors.orangeAccent,
+                foregroundColor: Colors.white,
+                minimumSize: const Size(double.infinity,50),
+                shape:RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                ),
+              ),
+              child: const Text('BOOK THIS NOW'),
+            ),
+          ),
+        ],
+      ),
+      appBar: AppBar(
+        title: Text(hotel.name),
+        actions: const [
+
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.orangeAccent,
+                    ),
         ],
       ),
     );
