b7bd3d03c0483b71bdab94ecc4bebaaf2c918e86
Yahya <takeyahya6@gmail.com>
2024-07-17 00:02:51 +0600

    Carousel slider added

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,15 +1,11 @@
-/*
- * @author: ARDhruvo
-*/
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hotel_khujo/Hotel/HotelDetailPageC.dart';
 import 'package:hotel_khujo/Hotel/hotel.dart';
 import 'package:hotel_khujo/MyHomePageC.dart';
-import 'package:dots_indicator/dots_indicator.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 
 class MyHomePage extends StatefulWidget {
   final String title;
@@ -24,44 +20,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  final List<String> _carouselImages = [];
-  final _firestoreInstance = FirebaseFirestore.instance;
-  var _dotPosition = 0;
-
-
-  fetchCarouselImages() async {
-    QuerySnapshot qn =
-    await _firestoreInstance.collection("slides").get();
-    setState(() {
-      for (int i = 0; i < qn.docs.length; i++) {
-        _carouselImages.add(
-          qn.docs[i]["slide-img"],
-        );
-        print(qn.docs[i]["slide-img"]);
-      }
-    });
-
+  Future<List<DocumentSnapshot>> getimgfromFirebase() async {
+    var firestore = FirebaseFirestore.instance;
+    QuerySnapshot qn = await firestore.collection("slides").get();
     return qn.docs;
   }
 
   @override
-  void initState() {
-    fetchCarouselImages();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-
-    final MyHomePageC a=Get.put(MyHomePageC());
+    final MyHomePageC a = Get.put(MyHomePageC());
 
     return Scaffold(
       backgroundColor: Colors.grey[300],
@@ -70,112 +37,98 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.deepOrangeAccent,
         actions: [
           IconButton(
-              onPressed: () {
-                showSearch(
-                    context: context,
-                    delegate: CustomSearchDelegate(),);
-              },
-              icon: const Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: CustomSearchDelegate(),
+              );
+            },
+            icon: const Icon(Icons.search),
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AspectRatio(
-                aspectRatio: 3.5,
-                child: CarouselSlider(
-                    items: _carouselImages
-                        .map((item) => Padding(
-                      padding: const EdgeInsets.only(left: 3, right: 3),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: NetworkImage(item),
-                                fit: BoxFit.fitWidth)),
-                      ),
-                    ))
-                        .toList(),
-                    options: CarouselOptions(
-                        autoPlay: false,
-                        enlargeCenterPage: true,
-                        viewportFraction: 0.8,
-                        enlargeStrategy: CenterPageEnlargeStrategy.height,
-                        onPageChanged: (val, carouselPageChangedReason) {
-                          setState(() {
-                            _dotPosition = val;
-                          });
-                        })),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-
-              DotsIndicator(
-                dotsCount:
-                _carouselImages.isEmpty ? 1 : _carouselImages.length,
-                position: _dotPosition.toDouble(),
-                decorator: const DotsDecorator(
-                  activeColor: Colors.deepOrange,
-                  color: Colors.deepOrange,
-                  spacing: EdgeInsets.all(2),
-                  activeSize: Size(8, 8),
-                  size: Size(6, 6),
-                ),
-              ),
-            ]
-        ),
+      body: FutureBuilder<List<DocumentSnapshot>>(
+        future: getimgfromFirebase(),
+        builder: (_, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No images found.'));
+          }
+          return CarouselSlider.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index, realIdx) {
+              DocumentSnapshot sliderImage = snapshot.data![index];
+              return Container(
+                child: Image.network(sliderImage['img']),
+              );
+            },
+            options: CarouselOptions(
+              autoPlay: true,
+              aspectRatio: 1.0,
+              enlargeCenterPage: true,
+            ),
+          );
+        },
       ),
       drawer: Drawer(
         child: Container(
-            color: const Color.fromRGBO(255,183,77, 1),
-            child: ListView(
-              children: [
-                DrawerHeader(child: Center(child: Image.asset('assets/logo1.png'),)),
-                ListTile(
-                  leading: const Icon(Icons.account_circle),
-                  title: const Text("Profile"),
-                  onTap: () {
-                    a.getToProfilePage();
-                  }, //Profile Page
+          color: const Color.fromRGBO(255, 183, 77, 1),
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Center(
+                  child: Image.asset('assets/logo1.png'),
                 ),
-                ListTile(
-                  leading: const Icon(Icons.assignment_turned_in),
-                  title: const Text("Booked"),
-                  onTap: () {}, //Booked Page
-                ),
-                ListTile(
-                  leading: const Icon(Icons.favorite_outlined),
-                  title: const Text("Favorites"),
-                  onTap: () {}, //Favorite Page
-                ),
-                ListTile(
-                  leading: const Icon(Icons.announcement),
-                  title: const Text("Helps and Services"),
-                  onTap: () {
-                    a.getToHelpPage();
-                  }, //Help Page
-                ),
-                ListTile(
-                  leading: const Icon(Icons.directions_run),
-                  title: const Text("Sign Out"),
-                  onTap: () {
-                    a.getToLoginPage();
-                  }, // Sign Out
-                ),
-              ],
-            )),
+              ),
+              ListTile(
+                leading: const Icon(Icons.account_circle),
+                title: const Text("Profile"),
+                onTap: () {
+                  a.getToProfilePage();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.assignment_turned_in),
+                title: const Text("Booked"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.favorite_outlined),
+                title: const Text("Favorites"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.announcement),
+                title: const Text("Helps and Services"),
+                onTap: () {
+                  a.getToHelpPage();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.directions_run),
+                title: const Text("Sign Out"),
+                onTap: () {
+                  a.getToLoginPage();
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-class CustomSearchDelegate extends SearchDelegate<String> {
 
-  final Hoteldetailpagec a=Get.put(Hoteldetailpagec());
+class CustomSearchDelegate extends SearchDelegate<String> {
+  final Hoteldetailpagec a = Get.put(Hoteldetailpagec());
 
   @override
-  List<IconButton> buildActions(BuildContext context) {
+  List<Widget>? buildActions(BuildContext context) {
     return [
       IconButton(
         icon: const Icon(Icons.clear),
@@ -188,7 +141,8 @@ class CustomSearchDelegate extends SearchDelegate<String> {
 
   @override
   Widget buildLeading(BuildContext context) {
-    return IconButton(icon: const Icon(Icons.arrow_back),
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
       onPressed: () {
         close(context, '');
       },
@@ -200,10 +154,8 @@ class CustomSearchDelegate extends SearchDelegate<String> {
     return _buildSearchResults(query);
   }
 
-
   @override
   Widget buildSuggestions(BuildContext context) {
-
     return FutureBuilder<List<String>>(
       future: _getSuggestions(query),
       builder: (context, snapshot) {
@@ -235,7 +187,6 @@ class CustomSearchDelegate extends SearchDelegate<String> {
   }
 
   Future<List<String>> _getSuggestions(String query) async {
-
     if (query.isEmpty) {
       return [];
     }
@@ -251,23 +202,27 @@ class CustomSearchDelegate extends SearchDelegate<String> {
     return querySnapshot.docs.map((doc) => doc['name'] as String).toList();
   }
 
-  Widget _buildSearchResults(String query){
+  Widget _buildSearchResults(String query) {
     return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('hotels').where('name',isGreaterThanOrEqualTo: query).where('name' ,isLessThan: query + 'z').snapshots(),
-      builder: (context,snapshot){
-        if(snapshot.connectionState == ConnectionState.waiting){
+      stream: FirebaseFirestore.instance
+          .collection('hotels')
+          .where('name', isGreaterThanOrEqualTo: query)
+          .where('name', isLessThan: query + 'z')
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
         }
 
-        if(snapshot.hasError){
-          return Center(child: Text('Error : ${snapshot.error}'));
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
         }
 
-        if(!snapshot.hasData || snapshot.data!.docs.isEmpty){
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
           return const Center(child: Text('No hotels found.'));
         }
 
-        List <Hotel> hotels = snapshot.data!.docs.map((doc){
+        List<Hotel> hotels = snapshot.data!.docs.map((doc) {
           return Hotel(
             id: doc.id,
             name: doc['name'],
@@ -278,21 +233,19 @@ class CustomSearchDelegate extends SearchDelegate<String> {
         }).toList();
 
         return ListView.builder(
-            itemCount: hotels.length,
-            itemBuilder: (context,index){
-              Hotel hotel =hotels[index];
-              return ListTile(
-                title: Text(hotel.name),
-                leading: Image.network(hotel.imageUrl),
-                onTap: (){
-                  a.getToHotelDetailPage(hotel);
-                },
-              );
-            },
+          itemCount: hotels.length,
+          itemBuilder: (context, index) {
+            Hotel hotel = hotels[index];
+            return ListTile(
+              title: Text(hotel.name),
+              leading: Image.network(hotel.imageUrl),
+              onTap: () {
+                a.getToHotelDetailPage(hotel);
+              },
+            );
+          },
         );
-
       },
     );
   }
-
 }
