74413b0419b05239ebfa4cdee66b6ecfb0deedfd
Yahya <takeyahya6@gmail.com>
2024-09-18 22:19:23 +0600

    product in home page

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -20,10 +20,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final Hoteldetailpagec a = Get.put(Hoteldetailpagec());
+  final FirebaseFirestore _firestoreInstance = FirebaseFirestore.instance;
+  final List _products = [];
+
   Future<List<DocumentSnapshot>> getimgfromFirebase() async {
-    var firestore = FirebaseFirestore.instance;
-    QuerySnapshot qn = await firestore.collection("slides").get();
+    QuerySnapshot qn = await _firestoreInstance.collection("slides").get();
     return qn.docs;
+  }
+
+  fetchProducts() async {
+    QuerySnapshot qn = await _firestoreInstance.collection("hotels").get();
+    setState(() {
+      for (int i = 0; i < qn.docs.length; i++) {
+        _products.add({
+          "product-name": qn.docs[i]["name"],
+          "product-img": qn.docs[i]["imageUrl"],
+        });
+      }
+    });
+    return qn.docs;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchProducts(); // Fetch products at the start
   }
 
   @override
@@ -47,33 +69,68 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: FutureBuilder<List<DocumentSnapshot>>(
-        future: getimgfromFirebase(),
-        builder: (_, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No images found.'));
-          }
-          return CarouselSlider.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index, realIdx) {
-              DocumentSnapshot sliderImage = snapshot.data![index];
-              return Container(
-                child: Image.network(sliderImage['img']),
+      body: Column(
+        children: [
+          FutureBuilder<List<DocumentSnapshot>>(
+            future: getimgfromFirebase(),
+            builder: (_, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              }
+              if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Center(child: Text('No images found.'));
+              }
+              return CarouselSlider.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index, realIdx) {
+                  DocumentSnapshot sliderImage = snapshot.data![index];
+                  return Image.network(sliderImage['img']);
+                },
+                options: CarouselOptions(
+                  autoPlay: true,
+                  aspectRatio: 1,
+                  enlargeCenterPage: true,
+                ),
               );
             },
-            options: CarouselOptions(
-              autoPlay: true,
-              aspectRatio: 1.0,
-              enlargeCenterPage: true,
+          ),
+          const SizedBox(height: 5),
+          Expanded(
+            child: GridView.builder(
+              itemCount: _products.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1,
+              ),
+              itemBuilder: (_, index) {
+                return GestureDetector(
+
+                  child: Card(
+                    elevation: 3,
+                    child: Column(
+                      children: [
+                        AspectRatio(
+                          aspectRatio: 2,
+                          child: Container(
+                            color: Colors.deepOrangeAccent,
+                            child: Image.network(
+                              _products[index]["product-img"],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Text(_products[index]["product-name"]),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       ),
       drawer: Drawer(
         child: Container(
